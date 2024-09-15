c2cc9ee0da904c02ddbdb622d8ebc50627c48237
Sinan Talha KOSAR <sinantalhakosar@gmail.com>
2024-09-16 00:20:38 +0300

    feat: Add search input to chat list

--- components/dashboard/ChatList.tsx
+++ components/dashboard/ChatList.tsx
@@ -3,8 +3,10 @@
 import { createClient } from "@/utils/supabase/client";
 import { useDashboard } from "../../contexts/DashboardContext";
 import { useCallback, useEffect, useState } from "react";
-import { ChatType, Provider } from "@/types/Common.types";
+import { ChatType, MessageType, Provider } from "@/types/Common.types";
 import { Chat } from "./Chat";
+import { Input } from "../ui/input";
+import { Search } from "lucide-react";
 
 export const ChatList = () => {
   const supabase = createClient();
@@ -51,15 +53,28 @@ export const ChatList = () => {
   };
 
   return (
-    <div className="flex flex-col gap-1 h-full overflow-y-scroll my-2 p-2">
-      {chatList.map((chat) => (
-        <Chat
-          key={chat.id}
-          chat={chat}
-          onClick={handleChatClick}
-          active={chat.id === selectedChatId}
-        />
-      ))}
+    <div className="flex flex-col h-full overflow-y-scroll p-6 w-1/3">
+      <div className="mb-4">
+        <div className="relative">
+          <Input
+            type="text"
+            placeholder="Search chats..."
+            className="w-full dark:bg-[#2f333c] rounded-2xl h-14 pl-10" // Added left padding for the icon
+            onChange={(e) => {}}
+          />
+          <Search className="h-5 w-5 absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+        </div>
+      </div>
+      <div className="flex flex-col gap-2">
+        {chatList.map((chat) => (
+          <Chat
+            key={chat.id}
+            chat={chat}
+            onClick={handleChatClick}
+            active={chat.id === selectedChatId}
+          />
+        ))}
+      </div>
     </div>
   );
 };
