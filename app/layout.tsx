227d021c04aa587b30f87bbfc605c6677409c0bd
Sinan Talha KOSAR <sinantalhakosar@gmail.com>
2024-09-15 11:04:33 +0300

    refactor: Improve UI for homepage and dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,20 +32,24 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-4 sm:gap-20 items-center">
+            <div className="flex-1 w-full flex flex-col gap-4 sm:gap-2 items-center">
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold text-xl">
-                    <Link href={"/"} className="text-[#FE8621]">HOME</Link>
+                    <Link href={"/"} className="text-[#FE8621]">
+                      HOME
+                    </Link>
                   </div>
                   <HeaderAuth />
                 </div>
               </nav>
-              <div className="flex flex-col gap-20 max-w-5xl p-5">
-                {children}
+              <div className="flex flex-col w-full h-[calc(100vh-10rem)] overflow-hidden items-center">
+                <div className="flex-grow overflow-auto px-5 w-full flex items-center justify-center">
+                  {children}
+                </div>
               </div>
 
-              <footer className="w-full flex flex-col items-center justify-center border-t mx-auto text-center text-xs gap-2 py-16 mt-auto">
+              <footer className="w-full flex flex-col items-center justify-center border-t mx-auto text-center text-xs gap-2 py-4 mt-auto">
                 <p className="flex items-center gap-2">
                   <Copyright className="w-4 h-4" /> Copyright 2024{" "}
                   <Link
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import Hero from "@/components/hero";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 
 export default async function Index() {
   return (
-    <>
+    <div className="flex flex-col h-full">
       <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4"></main>
-    </>
+    </div>
   );
 }
